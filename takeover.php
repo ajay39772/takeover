c701098830791784d95a96ea34cc4fd6b8aceefc
ajay39772 <43997292+ajay39772@users.noreply.github.com>
2021-01-14 14:26:11 +0530

    Update and rename takeover.html to takeover.php

---
+++ takeover.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Safe Takeover</title>
+</head>
+<body>
+<?php echo '<h1>Hello World Hi I was able to takeover this domain takeover by hacker2202</h1>'; ?> 
+
+</body>
+</html>
